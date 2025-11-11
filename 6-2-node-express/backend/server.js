7ce225185681f4471cf11a18bed6b35184940b33
TurkiAlslamah <s202172030@kfupm.edu.sa>
2025-11-11 19:51:45 +0300

    todo2

--- 6-2-node-express/backend/server.js
+++ 6-2-node-express/backend/server.js
@@ -4,8 +4,19 @@ import { getRandomQuote } from "./quotes.js";
 
 {/*write code for cors*/}
 
+const app = express();
+const PORT = 3000;
 
-{/*write code to define routes*/}
 
+{/*write code to define routes .... */}
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
+app.use(cors());
 
 {/*write code to create server*/}
+app.get("/api/quote", (req, res) => {
+  const quote = getRandomQuote();
+  res.json({ quote });
+});
